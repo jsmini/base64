20aeaab32e5fea5a9150ff2298a41a3ee8878fe3
颜海镜 <yanhaijing@yeah.net>
2019-03-02 23:35:10 +0800

    0.2.0

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export as namespace jsminiBase64;
+
+export function encode (str: string): string;
+export function decode (base64Str: string): string;
+export function atob (str: string): string;
+export function btoa (base64Str: string): string;
